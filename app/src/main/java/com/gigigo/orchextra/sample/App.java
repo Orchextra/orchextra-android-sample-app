0c475544db5ac57224e33d38fb2840760f95b5c0
Jose Benito <jfbenitopa@gmail.com>
2016-02-01 10:56:30 +0100

    Version name with two decimals is needed by server
    Added orchextra valid token

--- app/src/main/java/com/gigigo/orchextra/sample/App.java
+++ app/src/main/java/com/gigigo/orchextra/sample/App.java
@@ -10,6 +10,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Orchextra.sdkInitialize(getApplicationContext(), "Hello", "World");
+        Orchextra.sdkInitialize(getApplicationContext(), "e4b4cb3331f8c1432ab841e49b7c0609ed94c483", "7de522cef430f53c46dc508cc16c69657dc32f6f");
     }
 }
