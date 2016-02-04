ab0352bea942396d6172ed5d7a9b8976b7d499ce
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2016-02-04 17:53:23 +0100

    Added all pieces from beacon POC, ready for refactor. NOW compiles

--- app/src/main/java/com/gigigo/orchextra/sample/App.java
+++ app/src/main/java/com/gigigo/orchextra/sample/App.java
@@ -3,13 +3,23 @@ package com.gigigo.orchextra.sample;
 import android.app.Application;
 
 import com.gigigo.orchextra.Orchextra;
+import com.gigigo.orchextra.initalization.OrchextraCompletionCallback;
 
-public class App extends Application {
+public class App extends Application implements OrchextraCompletionCallback {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Orchextra.sdkInitialize(getApplicationContext(), "e4b4cb3331f8c1432ab841e49b7c0609ed94c483", "7de522cef430f53c46dc508cc16c69657dc32f6f");
+        Orchextra.sdkInitialize(this , "e4b4cb3331f8c1432ab841e49b7c0609ed94c483",
+            "7de522cef430f53c46dc508cc16c69657dc32f6f", this);
+    }
+
+    @Override public void onSuccess() {
+
+    }
+
+    @Override public void onError(String s) {
+
     }
 }
