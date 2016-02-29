ea69600f751fa6afb15cbf1d38a0336847c86110
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2016-02-29 18:41:36 +0100

    update app

--- app/src/main/java/com/gigigo/orchextra/sample/App.java
+++ app/src/main/java/com/gigigo/orchextra/sample/App.java
@@ -11,7 +11,7 @@ public class App extends Application implements OrchextraCompletionCallback {
     @Override
     public void onCreate() {
         super.onCreate();
-        Orchextra.sdkInitialize(this , "51318cdb73dc01a329ef9a04f155d744ffacedfe", "c29fd5e7d09c24474b74b297a3e7eef57cd1d2bc", this);
+        Orchextra.sdkInitialize(this , "240f87d92474068b4ff8c3913f828212b0e16108", "768d071ce2db5af45725ea791d0e092a6045b955", this);
     }
 
     @Override public void onSuccess() {
