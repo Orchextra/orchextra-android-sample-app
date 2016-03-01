21d54f55d42bed6a11ef06eafc1f60aa99ac09a5
Jose Benito <jfbenitopa@gmail.com>
2016-03-01 10:20:15 +0100

    Make changes to send the crime

--- app/src/main/java/com/gigigo/orchextra/sample/App.java
+++ app/src/main/java/com/gigigo/orchextra/sample/App.java
@@ -5,28 +5,30 @@ import android.app.Application;
 import com.gigigo.ggglogger.GGGLogImpl;
 import com.gigigo.orchextra.Orchextra;
 import com.gigigo.orchextra.domain.abstractions.actions.CustomSchemeReceiver;
-import com.gigigo.orchextra.domain.abstractions.initialization.OrchextraCompletionCallback;
+import com.gigigo.orchextra.sdk.model.ORCUser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
 
 
-public class App extends Application implements OrchextraCompletionCallback {
+public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Orchextra.sdkInitialize(this, "f7ae1c1bb7a556fdc9b517706d6f7a0c041334d9", "1a9e4aeadc38b3234b0cd119279ddfb1d9bf7f28", this);
+        Orchextra.sdkInitialize(this, "f7ae1c1bb7a556fdc9b517706d6f7a0c041334d9", "1a9e4aeadc38b3234b0cd119279ddfb1d9bf7f28", null);
+
         Orchextra.setCustomSchemeReceiver(new CustomSchemeReceiver() {
             @Override
             public void onReceive(String scheme) {
                 GGGLogImpl.log("SCHEME: " + scheme);
             }
         });
-    }
 
-    @Override public void onSuccess() {
-
-    }
-
-    @Override public void onError(String s) {
-
+        Orchextra.setUser(new ORCUser("1234567890",
+                new GregorianCalendar(1990, 10, 13),
+                ORCUser.Gender.ORCGenderMale,
+                new ArrayList<>(Arrays.asList("keyword1", "keyword2"))));
     }
 }
